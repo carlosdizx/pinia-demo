bb07bea7bd6871a31c5196d828e28293e2465a04
Carlos Diaz <carlos.diaz@ias.com.co>
2022-09-06 19:41:35 -0500

    Creado el index para manejar las rutas y creado un ejemplo en el App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1>Hola mundo</h1>
+  <router-link to="/">Home</router-link>
+  <br />
+  <router-link to="/counter">Counter</router-link>
+  <router-view></router-view>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
