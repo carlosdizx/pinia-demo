14ec0d471ff341d5e1a16d66e58ed4245c0ddc63
Carlos Diaz <carlos.diaz@ias.com.co>
2022-09-19 21:24:24 -0500

    Eliminando archivos sin uso y isntalado prettier y configurado para vue y typescript

--- src/domain/model/task/TaskRepository.ts
+++
@@ -1,4 +0,0 @@
-import { Task } from "./Task";
-import { CrudFirestoreOperations } from "../common/CrudFirestoreOperations";
-
-export interface TaskRepository extends CrudFirestoreOperations<Task, string> {}
--- src/infrastructure/database/task/TaskDataRepository.ts
+++
@@ -1,5 +0,0 @@
-import { CrudDataFirestoreOperations } from "../../common/CrudDataFirestoreOperations";
-import { TaskData } from "./TaskData";
-
-export interface TaskDataRepository
-  extends CrudDataFirestoreOperations<TaskData, string> {}
