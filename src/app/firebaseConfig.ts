86d7766eed9a73a1b0904fc2c4cc40a4c8bc560d
dacho7 <davidtobarst@gmail.com>
2022-09-19 22:05:40 -0500

    variables de entorno vite

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -1,25 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-/*
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
-};
- */
+const env = import.meta.env
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV-93Obborno_7fczAd7CNkNOFib2qybc",
-  authDomain: "pinia-app.firebaseapp.com",
-  projectId: "pinia-app",
-  storageBucket: "pinia-app.appspot.com",
-  messagingSenderId: "515111131981",
-  appId: "1:515111131981:web:15d6472b1adb00788d7504",
-  measurementId: "G-K3JQRMBF1V",
+  apiKey: env.VITE_APP_API_KEY,
+  authDomain: env.VITE_APP_AUTH_DOMAIN,
+  projectId: env.VITE_APP_PROJECT_ID,
+  storageBucket: env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: env.VITE_APP_APP_ID,
+  measurementId: env.VITE_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
