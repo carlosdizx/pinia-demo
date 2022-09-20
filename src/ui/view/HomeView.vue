da0c0c99851560ac42945e4b0db3cddd9d10c2ac
Carlos Diaz <carlos.diaz@ias.com.co>
2022-09-20 13:51:52 -0500

    Funcionando el servicio de autenticacion, singIn y singUp

--- src/ui/view/HomeView.vue
+++ src/ui/view/HomeView.vue
@@ -6,14 +6,19 @@
   <button @click="buscar">Buscar!!</button>
   <button @click="actualizar">Actualizar!!</button>
   <button @click="eliminar">Eliminar!!</button>
+  <hr />
+  <button @click="singUp">SinUp</button>
+  <button @click="singIn">SinIn</button>
 </template>
 
-<script lang="ts" setup >
+<script lang="ts" setup>
 import { PersonDataAdapter } from "../../infrastructure/database/person/PersonDataAdapter";
 import { useCounterStore } from "../../infrastructure/store/counter";
 import { Person } from "../../domain/model/person/Person";
+import { AuthService } from "../../infrastructure/auth/AuthService";
 
 const personAdapter: PersonDataAdapter = new PersonDataAdapter();
+const auth = new AuthService();
 
 const guardar = async () => {
   const person: Person = new Person().map({ id: "1", name: "xd" });
@@ -42,6 +47,19 @@ const actualizar = async () => {
 const eliminar = () => {
   personAdapter.delete("HaEqsKdaeZD0NpBcStgv");
   console.log("elimiando id:", "HaEqsKdaeZD0NpBcStgv");
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+const singUp = () => {
+  console.log(auth.currentEmailUser());
+  auth.singUp("carlosbiche98@gmail.com", "12345678");
+  console.log(auth.currentEmailUser());
+};
+
+const singIn = () => {
+  console.log(auth.currentEmailUser());
+  auth.singIn("carlosbiche98@gmail.com", "12345678");
+  console.log(auth.currentEmailUser());
 };
 
 const counterStore = useCounterStore();
